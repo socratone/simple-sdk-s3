36a549ee8995157a656957770cf67450d4af8e4e
socratone <gim2origin@gmail.com>
2020-12-14 11:31:14 +0900

    인자 이름 수정

--- putFile.js
+++ putFile.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const fs = require('fs');
 const AWS = require('aws-sdk');
 
-const putFile = (path, name) => {
+const putFile = (file, key) => {
   AWS.config.update({
     region: 'ap-northeast-2',
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -12,8 +12,8 @@ const putFile = (path, name) => {
   const upload = new AWS.S3.ManagedUpload({
     params: {
       Bucket: process.env.AWS_S3_BUCKET_NAME,
-      Key: name,
-      Body: fs.createReadStream(path),
+      Key: key,
+      Body: fs.createReadStream(file),
       ACL: 'public-read'
     }
   });
